046e8292a91d5a63da9ab38ef35fc7ea151696e9
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2025-11-23 00:44:11 +0100

    Add get, set, insert, remove and reverse methods in SinglyLinkedList

--- DS_TS/SinglyLinkedList/SinglyLinkedList.js
+++ DS_TS/SinglyLinkedList/SinglyLinkedList.js
@@ -1,5 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Złożoność:
 // - push
 // Czas: O(1) - Wykonuję stałą liczbę operacji
@@ -13,6 +12,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // - unshift
 // Czas: O(1) - Wykonuję stałą liczbę operacji
 // Pamięć: O(1) - tylko zmienne pomocnicze
+// - get
+// Czas: O(n) - Muszę przejść przez listę, aby dotrzeć do szukanego elementu.
+// Pamięć: O(1) - tylko zmienne pomocnicze
+// - set
+// Czas: O(n) - Muszę przejść przez listę, aby dotrzeć do szukanego elementu.
+// Pamięć: O(1) - tylko zmienne pomocnicze
+// - insert
+// Jeśli wykorzystuje metody unshift albo push to przyjmuje ich złożoność.
+// Czas: O(n) - Muszę przejść przez listę, aby dotrzeć do szukanego elementu.
+// Pamięć: O(1) - tylko zmienne pomocnicze
+// - remove
+// Jeśli wykorzystuje metody shift albo pop to przyjmuje ich złożoność.
+// Czas: O(n) - Muszę przejść przez listę, aby dotrzeć do szukanego elementu.
+// Pamięć: O(1) - tylko zmienne pomocnicze
+// - reverse
+// Czas: O(n) - Jedna iteracja
+// Pamięć: O(1) - tylko zmienne pomocnicze
+Object.defineProperty(exports, "__esModule", { value: true });
 class Node {
     value;
     next;
@@ -42,20 +59,20 @@ class SinglyLinkedList {
     pop() {
         if (!this.head)
             return undefined;
-        let current = this.head;
-        let prev = this.head;
-        while (current.next) {
-            prev = current;
-            current = current.next;
+        let currentNode = this.head;
+        let prevNode = this.head;
+        while (currentNode.next) {
+            prevNode = currentNode;
+            currentNode = currentNode.next;
         }
-        prev.next = null;
-        this.tail = prev;
+        prevNode.next = null;
+        this.tail = prevNode;
         this.length--;
         if (this.length === 0) {
             this.head = null;
             this.tail = null;
         }
-        return current;
+        return currentNode;
     }
     shift() {
         if (!this.head)
@@ -80,6 +97,77 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+    get(index) {
+        if (index < 0 || index >= this.length)
+            return undefined;
+        let currentNode = this.head;
+        let i = 0;
+        while (i < index) {
+            currentNode = currentNode.next;
+            i++;
+        }
+        return currentNode;
+    }
+    set(index, newValue) {
+        const node = this.get(index);
+        if (!node)
+            return false;
+        node.value = newValue;
+        return true;
+    }
+    insert(index, value) {
+        if (index < 0 || index > this.length)
+            return false;
+        if (index === this.length) {
+            this.push(value);
+        }
+        else if (index === 0) {
+            this.unshift(value);
+        }
+        else {
+            const newNode = new Node(value);
+            const prevNode = this.get(index - 1);
+            if (!prevNode)
+                return false;
+            newNode.next = prevNode.next;
+            prevNode.next = newNode;
+            this.length++;
+        }
+        return true;
+    }
+    remove(index) {
+        if (index < 0 || index >= this.length)
+            return undefined;
+        if (index === 0)
+            return this.shift();
+        else if (index === this.length - 1)
+            return this.pop();
+        else {
+            const prevNode = this.get(index - 1);
+            if (!prevNode || !prevNode.next)
+                return undefined;
+            const nodeToRemove = prevNode.next;
+            prevNode.next = nodeToRemove.next;
+            this.length--;
+            return nodeToRemove;
+        }
+    }
+    reverse() {
+        if (!this.head || !this.tail)
+            return this;
+        let currentNode = this.head;
+        this.head = this.tail;
+        this.tail = currentNode;
+        let prevNode = null;
+        while (currentNode.next) {
+            let nextNode = currentNode.next;
+            currentNode.next = prevNode;
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+        this.head.next = prevNode;
+        return this;
+    }
 }
 const singlyLinkedList = new SinglyLinkedList();
 console.log(singlyLinkedList.push(1));
@@ -87,3 +175,12 @@ console.log(singlyLinkedList.push(2));
 console.log(singlyLinkedList.pop());
 console.log(singlyLinkedList.shift());
 console.log(singlyLinkedList.unshift(4));
+console.log(singlyLinkedList.unshift(5));
+console.log(singlyLinkedList.unshift(6));
+console.log(singlyLinkedList.get(2));
+console.log(singlyLinkedList.set(2, 7));
+console.log(singlyLinkedList.get(2));
+console.log(singlyLinkedList.insert(0, 10));
+console.log(singlyLinkedList.get(0));
+console.log(singlyLinkedList.remove(1));
+console.log(singlyLinkedList.reverse());
--- DS_TS/SinglyLinkedList/SinglyLinkedList.ts
+++ DS_TS/SinglyLinkedList/SinglyLinkedList.ts
@@ -11,6 +11,24 @@
 // - unshift
 // Czas: O(1) - Wykonuję stałą liczbę operacji
 // Pamięć: O(1) - tylko zmienne pomocnicze
+// - get
+// Czas: O(n) - Muszę przejść przez listę, aby dotrzeć do szukanego elementu.
+// Pamięć: O(1) - tylko zmienne pomocnicze
+// - set
+// Czas: O(n) - Muszę przejść przez listę, aby dotrzeć do szukanego elementu.
+// Pamięć: O(1) - tylko zmienne pomocnicze
+// - insert
+// Jeśli wykorzystuje metody unshift albo push to przyjmuje ich złożoność.
+// Czas: O(n) - Muszę przejść przez listę, aby dotrzeć do szukanego elementu.
+// Pamięć: O(1) - tylko zmienne pomocnicze
+// - remove
+// Jeśli wykorzystuje metody shift albo pop to przyjmuje ich złożoność.
+// Czas: O(n) - Muszę przejść przez listę, aby dotrzeć do szukanego elementu.
+// Pamięć: O(1) - tylko zmienne pomocnicze
+// - reverse
+// Czas: O(n) - Jedna iteracja
+// Pamięć: O(1) - tylko zmienne pomocnicze
+
 class Node<T> {
   constructor(public value: T, public next: Node<T> | null = null) {}
 }
@@ -34,20 +52,20 @@ class SinglyLinkedList<T> {
   // Implementacja metody pop po sprawdzeniu
   public pop(): Node<T> | undefined {
     if (!this.head) return undefined;
-    let current = this.head;
-    let prev = this.head;
-    while (current.next) {
-      prev = current;
-      current = current.next;
+    let currentNode = this.head;
+    let prevNode = this.head;
+    while (currentNode.next) {
+      prevNode = currentNode;
+      currentNode = currentNode.next;
     }
-    prev.next = null;
-    this.tail = prev;
+    prevNode.next = null;
+    this.tail = prevNode;
     this.length--;
     if (this.length === 0) {
       this.head = null;
       this.tail = null;
     }
-    return current;
+    return currentNode;
   }
   public shift(): Node<T> | undefined {
     if (!this.head) return undefined;
@@ -67,6 +85,66 @@ class SinglyLinkedList<T> {
       this.head = newNode;
     }
     this.length++;
+    return this;
+  }
+  public get(index: number): Node<T> | undefined {
+    if (index < 0 || index >= this.length) return undefined;
+    let currentNode = this.head;
+    let i = 0;
+    while (i < index) {
+      currentNode = currentNode!.next;
+      i++;
+    }
+    return currentNode!;
+  }
+  public set(index: number, newValue: T): boolean {
+    const node = this.get(index);
+    if (!node) return false;
+    node.value = newValue;
+    return true;
+  }
+  public insert(index: number, value: T): boolean {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) {
+      this.push(value);
+    } else if (index === 0) {
+      this.unshift(value);
+    } else {
+      const newNode = new Node(value);
+      const prevNode = this.get(index - 1);
+      if (!prevNode) return false;
+      newNode.next = prevNode.next;
+      prevNode.next = newNode;
+      this.length++;
+    }
+    return true;
+  }
+  public remove(index: number): Node<T> | undefined {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    else if (index === this.length - 1) return this.pop();
+    else {
+      const prevNode = this.get(index - 1);
+      if (!prevNode || !prevNode.next) return undefined;
+      const nodeToRemove = prevNode.next;
+      prevNode.next = nodeToRemove.next;
+      this.length--;
+      return nodeToRemove;
+    }
+  }
+  public reverse(): SinglyLinkedList<T> {
+    if (!this.head || !this.tail) return this;
+    let currentNode = this.head;
+    this.head = this.tail;
+    this.tail = currentNode;
+    let prevNode = null;
+    while (currentNode.next) {
+      let nextNode = currentNode.next;
+      currentNode.next = prevNode;
+      prevNode = currentNode;
+      currentNode = nextNode;
+    }
+    this.head.next = prevNode;
     return this;
   }
   // Moja pierwsza implementacja metody pop
@@ -97,3 +175,12 @@ console.log(singlyLinkedList.push(2));
 console.log(singlyLinkedList.pop());
 console.log(singlyLinkedList.shift());
 console.log(singlyLinkedList.unshift(4));
+console.log(singlyLinkedList.unshift(5));
+console.log(singlyLinkedList.unshift(6));
+console.log(singlyLinkedList.get(2));
+console.log(singlyLinkedList.set(2, 7));
+console.log(singlyLinkedList.get(2));
+console.log(singlyLinkedList.insert(0, 10));
+console.log(singlyLinkedList.get(0));
+console.log(singlyLinkedList.remove(1));
+console.log(singlyLinkedList.reverse());
